ae09a091f6b6ad7989b67d1bd8f5d91cdc3b3787
User <paashtetik@gmail.com>
2025-02-11 11:24:44 +0300

    O.and.C

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private String title;
+    private int publicationYear;
+    private Author author;
+
+    public Book(String title, Author author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Author Tolstoy = new Author("Лев", "Толстой");
+        Author Pushkin = new Author("Александр", "Пушкин");
+        Book Karenina = new Book("Анна Каренина", Tolstoy, 1877);
+        Book Onegin = new Book("Евгений Онегин", Pushkin, 1830);
+
+        System.out.println("Книга 1: " + Karenina.getTitle() + " (" + Karenina.getAuthor().getFirstName() + " " + Karenina.getAuthor().getLastName() + ", " + Karenina.getPublicationYear() + ")");
+        System.out.println("Книга 2: " + Onegin.getTitle() + " (" + Onegin.getAuthor().getFirstName() + " " + Onegin.getAuthor().getLastName() + ", " + Onegin.getPublicationYear() + ")");
+
+        Onegin.setPublicationYear(1831);
+        System.out.println("Книга 2 (после изменения года публикации): " + Onegin.getTitle() + " (" + Onegin.getAuthor().getFirstName() + " " + Onegin.getAuthor().getLastName() + ", " + Onegin.getPublicationYear() + ")");
     }
 }
